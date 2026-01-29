16d569cc33b5c4a26519689fff2e25c40f01656e
abhirajsingh <abhirajsinghchouhan4@gmail.com>
2026-01-29 17:55:51 +0530

    initial setup

---
+++ secured-blog-platform/src/main/java/com/abhiraj/blog/SecuredBlogPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.abhiraj.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredBlogPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredBlogPlatformApplication.class, args);
+	}
+
+}
